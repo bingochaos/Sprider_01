16d96d6bbf662a537ef0949ae2570e6fe8dfd043
bingochaos <bingoc1992@163.com>
2016-04-12 22:56:32 +0800

    主函数

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,14 +1,18 @@
 package com.company;
 
+import apple.laf.JRSUIUtils;
+
+import java.awt.peer.LabelPeer;
 import java.util.ArrayList;
+import java.util.TreeMap;
 
 
-public class Main {
+public class Main  {
 
     public static String url = "http://bj.lianjia.com/xiaoqu/1111027381403/esf/";
     //public static String url = "http://bj.lianjia.com/ershoufang/pg1";
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
         String result =  new SprideHttpGet().httpGet(url);
         ArrayList<HouseInfo> houseInfos = new ArrayList<HouseInfo>();
@@ -16,18 +20,14 @@ public class Main {
         //RegexUrl.getRegexUrlSington().RegexForHouses(result, MyRegularExpression.houseInfoExpression);
         if(pageNum != null)
         {
-            for(int i = 1; i < pageNum.getPageNums()+1; i++)
-            {
+            for(int i = 1; i < pageNum.getPageNums()+1; i++) {
                 PageSpride pageSpride = new PageSpride();
                 pageSpride.setUrl(pageNum.getPageUrl());
                 pageSpride.setPageNum(i);
-                new Thread(pageSpride).start();
-
+                Thread thread = new Thread(pageSpride);
+                thread.start();
             }
         }
-
-
-
     }
 
 
